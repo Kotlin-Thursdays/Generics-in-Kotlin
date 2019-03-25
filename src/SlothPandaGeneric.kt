24477eba9d7b40b88680f8b58c6c49293164583a
Amanda Hinchman-Dominguez <hinchman17@grinnell.edu>
2019-03-25 13:07:10 -0500

    Generics in Kotlin part 2: Higher-order functions

--- src/SlothPandaGeneric.kt
+++ src/SlothPandaGeneric.kt
@@ -1,11 +1,62 @@
-open class Mammal(val name: String) {
+import kotlin.random.Random
+import kotlin.reflect.KFunction1
+
+sealed class Mammal(val name: String) {
     fun eat() {}
     fun sleep() {}
+    fun swim() {
+        println("${name.toUpperCase()} CAN SWIM")
+    }
+    open fun relief() {}
 }
 
-data class Sloths(val slothName: String, val isTwoFingered: Boolean): Mammal(slothName)
+data class Sloths(val slothName: String,
+                  val isTwoFingered: Boolean,
+                  var slothWeight: Int): Mammal(slothName) {
+    override fun relief() {
+        val oldWeight = slothWeight
+        val weightShed = Random.nextInt(0, slothWeight/3)
+        val newWeight = slothWeight - weightShed
+        println("${slothName.toUpperCase()} FINALLY WENT THIS WEEK")
+        println("\tOld weight: $oldWeight \t|\t New weight: $newWeight")
+    }
+}
 
 data class Pandas(val pandaName: String) : Mammal(pandaName)
+
+data class Manatee(val manateeName: String): Mammal(manateeName)
+
+fun Mammal.vertebraeCount(): Int {
+    return when (this) {
+        is Manatee -> 6
+        is Sloths -> 10
+        else -> 7
+    }
+}
+
+fun Mammal.knownSpeciesCount(): Int {
+    return when (this) {
+        is Sloths -> 6
+        is Pandas -> 2
+        is Manatee -> 3
+    }
+}
+
+fun Mammal.isEndangered(): Boolean {
+    return when (this) {
+        is Sloths -> true
+        is Pandas -> true
+        is Manatee -> false
+    }
+}
+
+fun mammalFactCheck(mammal: Mammal, factCheck: KFunction1<Mammal, Int>): Int {
+    return factCheck(mammal)
+}
+
+fun slothActivity(sloth: Sloths, action: Unit) {
+    sloth.run { action }
+}
 
 fun feedCrews(crew: List<Mammal>) {
     crew.forEach {
@@ -15,10 +66,16 @@ fun feedCrews(crew: List<Mammal>) {
 
 fun main() {
     val slothCrew = listOf(
-        Sloths("Jerry", false),
-        Sloths("Bae", true),
-        Sloths("Alex", false)
+        Sloths("Jerry", false, 15),
+        Sloths("Bae", true, 12),
+        Sloths("Alex", false, 15)
     )
+
+    slothCrew.forEach {
+        slothActivity(it, it.swim())
+        slothActivity(it, it.relief())
+    }
+
 
     val pandaCrew = listOf(
         Pandas("Tegan"),
@@ -29,12 +86,16 @@ fun main() {
     feedCrews(pandaCrew)
 
     val crewCrewCrew = listOf(
-        Sloths("Jerry", false),
-        Sloths("Bae", true),
-        Sloths("Alex", false),
+        Sloths("Jerry", false, 15),
         Pandas("Tegan"),
-        Pandas("Peggy")
+        Manatee("Manny")
     )
+
+    crewCrewCrew.forEach {
+        mammalFactCheck(it, Mammal::vertebraeCount)
+        mammalFactCheck(it, Mammal::knownSpeciesCount)
+        mammalFactCheck(it, Mammal::isEndangered)
+    }
 
     val compareByNames = Comparator { a: Mammal, b: Mammal ->
         a.name.first().toInt() - b.name.first().toInt()
